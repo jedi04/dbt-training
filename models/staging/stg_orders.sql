1ccdbc0f8d12ef40ec44a90533719dbf757c344f
nilaysheth@yopmail.com <nilaysheth@yopmail.com>
2022-08-11 19:10:05 +0000

    developing_stg_orders

---
+++ models/staging/stg_orders.sql
@@ -0,0 +1,22 @@
+SELECT 
+--from raw orders
+o.oderid,
+o.orderdate,
+o.shipdate,
+o.shipmode,
+o.ordersellingprice - o.ordercostprice as orderprofit,
+o.ordercostprice,
+o.ordersellingprice,
+-- from raw customer
+c.customername,
+c.segment,
+c.country,
+-- from raw product
+p.category,
+p.productname,
+p.subcategory
+from {{ ref('raw_orders') }} as o
+left join {{ref('raw_customer')}} as c 
+ON o.customerid = c.customerid
+left join {{ref('raw_product')}} as p
+ON o.productid = p.productid
